74ef88382e95f1f5a00efc5be862959af985d9c9
Mert UZAN <mertuzan203@gmail.com>
2019-01-04 19:37:09 +0300

    commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\harita\controllers\api;
+
+
+/**
+ * Default controller for the `harita` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
